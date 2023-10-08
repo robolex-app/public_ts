4f5402ec41952283ed25d571e02a88da2b722b04
Nicu Chiciuc <nicuchiciuc@gmail.com>
2023-10-08 15:55:00 +0300

    more comments

--- packages/jest-presets/jest/node/jest-preset.js
+++ packages/jest-presets/jest/node/jest-preset.js
@@ -1,6 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   roots: ['<rootDir>'],
+
+  /**
+    https://kulshekhar.github.io/ts-jest/docs/guides/esm-support
+    
+    To use ts-jest with ESM support:
+
+    Check ESM Jest documentation.
+    Enable useESM true for ts-jest config.
+    Include .ts in extensionsToTreatAsEsm Jest config option.
+    Ensure that tsconfig has module with value for ESM, e.g. ES2015 or ES2020 etc...
+   */
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
@@ -21,5 +32,13 @@ module.exports = {
     '<rootDir>/dist',
   ],
   preset: 'ts-jest',
+
+  /**
+    https://github.com/jestjs/jest/issues/13022#issuecomment-1183964334
+
+    Jest only implements Node module resolution.
+    In this case, you have to involve additional resolver.
+    For instance, ts-jest-resolver should do the job.
+   */
   resolver: 'ts-jest-resolver',
 }
