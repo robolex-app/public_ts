de0318ef61cce354d9f508f3d4f2b541eefbb5b0
Nicu Chiciuc <nicuchiciuc@gmail.com>
2023-10-08 15:53:14 +0300

    made tests work

--- packages/jest-presets/jest/node/jest-preset.js
+++ packages/jest-presets/jest/node/jest-preset.js
@@ -1,13 +1,25 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  roots: ["<rootDir>"],
+  roots: ['<rootDir>'],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: [
-    "<rootDir>/test/__fixtures__",
-    "<rootDir>/node_modules",
-    "<rootDir>/dist",
+    //
+    '<rootDir>/test/__fixtures__',
+    '<rootDir>/node_modules',
+    '<rootDir>/dist',
   ],
-  preset: "ts-jest",
-};
+  preset: 'ts-jest',
+  resolver: 'ts-jest-resolver',
+}
