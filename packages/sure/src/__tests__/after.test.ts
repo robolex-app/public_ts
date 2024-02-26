8fc19543d6a4d8c152ec8d3f3fd01c9d0ac374a8
Nicu Chiciuc <nicuchiciuc@gmail.com>
2024-02-26 23:01:50 +0200

    more tests

--- packages/sure/src/__tests__/after.test.ts
+++ packages/sure/src/__tests__/after.test.ts
@@ -1,5 +1,20 @@
 import { describe, it, expect } from 'vitest'
-import { good, bad, after, MetaObj, MetaNever, InferGood, InferBad, InferInput, InferMeta } from '../index.js'
+import {
+  good,
+  bad,
+  after,
+  MetaObj,
+  MetaNever,
+  InferGood,
+  InferBad,
+  InferInput,
+  InferMeta,
+  object,
+  string,
+  Sure,
+  Good,
+  Bad,
+} from '../index.js'
 import { assertEqual, assertIs } from './typeTestUtils.js'
 
 const sureNumber = (value: unknown) =>
@@ -48,6 +63,118 @@ const combined = after(sureNumber, positiveNum)
       first: typeof sureNumber
       second: typeof positiveNum
     }>
+  >(true)
+}
+
+/*
+Big Object
+ */
+
+const sureBig = after(
+  object({
+    age: after(sureNumber, val => {
+      if (val % 1 !== 0) return bad('not integer')
+      if (val < 0) return bad('not positive')
+      if (val > 200) return bad('too old, sorry')
+
+      return good(val)
+    }),
+    name: string,
+  }),
+  val => {
+    if (val.name === 'john' && val.age < 30) {
+      return bad({ name: `john's below 30 are too young` as const })
+    }
+
+    return good(val)
+  }
+)
+
+{
+  type InferredGood = InferGood<typeof sureBig>
+  type InferredBad = InferBad<typeof sureBig>
+  type InferredInput = InferInput<typeof sureBig>
+  type InferredMeta = InferMeta<typeof sureBig>
+
+  assertEqual<InferredInput, unknown>(true)
+  assertEqual<
+    InferredGood,
+    {
+      age: number
+      name: string
+    }
+  >(true)
+
+  assertEqual<
+    InferredBad,
+    | {
+        age?: 'not number' | 'not positive' | 'not integer' | 'too old, sorry'
+        name?: 'not string'
+      }
+    | {
+        name: "john's below 30 are too young"
+      }
+  >(true)
+
+  assertEqual<
+    InferredMeta,
+    {
+      meta: {
+        first: Sure<
+          {
+            age?: 'not number' | 'not integer' | 'not positive' | 'too old, sorry'
+            name?: 'not string'
+          },
+          {
+            age: number
+            name: string
+          },
+          unknown,
+          MetaObj<{
+            parent: typeof object
+            schema: {
+              age: Sure<
+                'not number' | 'not integer' | 'not positive' | 'too old, sorry',
+                number,
+                unknown,
+                MetaObj<{
+                  first: (value: unknown) => Good<number> | Bad<'not number'>
+                  second: (
+                    val: number
+                  ) => Good<number> | Bad<'not integer'> | Bad<'not positive'> | Bad<'too old, sorry'>
+                }>
+              >
+              name: Sure<'not string', string, unknown, MetaObj<undefined>>
+            }
+          }>
+        >
+
+        second: Sure<
+          {
+            name: "john's below 30 are too young"
+          },
+          {
+            age: number
+            name: string
+          },
+          {
+            age: number
+            name: string
+          },
+          MetaNever
+        >
+
+        // second: (val: {
+        //     age: number;
+        //     name: string;
+        // }) => Good<{
+        //     age: number;
+        //     name: string;
+        // }> | Bad<{
+        //     name: "john's below 30 are too young";
+        // }>;
+      }
+    }
   >(true)
 }
 
