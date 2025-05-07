854b42fefabbc4ae795d16cd8b469d890d532b23
Nicu Chiciuc <nicuchiciuc@gmail.com>
2025-05-07 20:57:52 +0300

    Ignore esm folder (#25)

--- packages/sure/esm/err.js
+++
@@ -1,8 +0,0 @@
-export const err = (schema) => (input) => {
-    const [good, result] = schema(input);
-    if (good) {
-        // @ts-expect-error Inferred as `unknown`
-        return result;
-    }
-    throw result;
-};
--- packages/sure/esm/intersection.d.ts
+++
@@ -1,6 +0,0 @@
-import { MetaNever, MetaObj, Sure } from './core.js';
-export declare function intersection<TFirstBad, TFirstGood extends Record<string, unknown>, TFirstInput, TFirstMeta extends MetaNever | MetaObj, TSecondBad, TSecondGood extends Record<string, unknown>, TSecondInput, TSecondMeta extends MetaNever | MetaObj>(first: Sure<TFirstBad, TFirstGood, TFirstInput, TFirstMeta>, second: Sure<TSecondBad, TSecondGood, TSecondInput, TSecondMeta>): Sure<TFirstBad | TSecondBad, TFirstGood & TSecondGood, TFirstInput & TSecondInput, MetaObj<{
-    first: typeof first;
-    second: typeof second;
-}>>;
-export declare const and: typeof intersection;
